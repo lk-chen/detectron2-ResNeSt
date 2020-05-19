6e7b898f7bf92989398600649c6c31875fe5052d
Zhen <lizhenpi@gmail.com>
2020-05-19 07:25:00 +0000

    Implement evaluation loss/precision report on eval_epoches

---
+++ detectron2/engine/kitti_trainer.py
@@ -0,0 +1,24 @@
+
+from detectron2.engine import DefaultTrainer
+from .hooks import LossEvalHook
+from detectron2.data import DatasetMapper, build_detection_test_loader
+
+class KittiTrainer(DefaultTrainer):
+    @classmethod
+    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+        if output_folder is None:
+            output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
+        return PascalVOCDetectionEvaluator(dataset_name, cfg, True, output_folder)
+
+    def build_hooks(self):
+        hooks = super().build_hooks()
+        hooks.insert(-1,LossEvalHook(
+            self.cfg.TEST.EVAL_PERIOD,
+            self.model,
+            build_detection_test_loader(
+                self.cfg,
+                self.cfg.DATASETS.TEST[0],
+                DatasetMapper(self.cfg,True)
+            )
+        ))
+        return hooks
