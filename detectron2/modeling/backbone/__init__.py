dd24660beaad622b869d71e53fd33d9545f4c097
Yijing Bai <everatnight@gmail.com>
2020-05-29 06:40:42 +0000

    fork NASFPN code and config

--- detectron2/modeling/backbone/__init__.py
+++ detectron2/modeling/backbone/__init__.py
@@ -3,6 +3,7 @@ from .build import build_backbone, BACKBONE_REGISTRY  # noqa F401 isort:skip
 
 from .backbone import Backbone
 from .fpn import FPN
+from .nasfpn import NASFPN
 from .resnet import ResNet, ResNetBlockBase, build_resnet_backbone, make_stage
 
 # TODO can expose more resnet blocks after careful consideration
